f2b11744aa59e22bf159f5c618e51f1ebeaac400
zineflix <zineflix0@gmail.com>
2025-06-14 20:07:46 +0800

    Update AdsPopunder.js

--- nextplay/ads/AdsPopunder.js
+++ nextplay/ads/AdsPopunder.js
@@ -5,17 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function triggerPopunder() {
-    openPopunder("https://raptripeessentially.com/a7mm18sw6?key=79442492f5fe436b0bc2484d9d0a8660");
+    openPopunder("https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3");
 }
 
 function openPopunder(url) {
     const urls = [
         url,
         "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
+        "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3"
     ];
 
     urls.forEach(adUrl => {
-        let popunder = window.open(adUrl, "_blank", "width=100,height=100,left=0,top=0");
+        let popunder = window.open(adUrl, "_blank", "width=1,height=1,left=0,top=0");
         if (popunder) {
             popunder.blur();
         }
