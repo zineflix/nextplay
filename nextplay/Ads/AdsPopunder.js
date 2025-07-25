84b8041ee13e899ea35b7d4527ef5b52f09d77a4
zineflix <zineflix0@gmail.com>
2025-07-25 13:19:23 +0800

    Update AdsPopunder.js

--- nextplay/Ads/AdsPopunder.js
+++ nextplay/Ads/AdsPopunder.js
@@ -1,4 +1,4 @@
-const popunderUrl = "https://veneeringanguishwhipped.com/yjq7ifj8w?key=4de5ae928742e8b8e44764a15993a110"; // Replace with your actual direct link
+const popunderUrl = "https://preoccupyray.com/atnj41y6?key=99e4a912dedac18eecb9005ee8e985b5"; // Replace with your actual direct link
 
 function openPopunder() {
   const features = "width=200,height=150,left=9999,top=9999";
