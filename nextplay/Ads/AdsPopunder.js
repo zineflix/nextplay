d6abfb7f08ff89cc29cf0b9222b9c8da65bf3683
zineflix <zineflix0@gmail.com>
2025-07-26 14:47:39 +0800

    Update AdsPopunder.js

--- nextplay/Ads/AdsPopunder.js
+++ nextplay/Ads/AdsPopunder.js
@@ -1,4 +1,4 @@
-const popunderUrl = "https://boredomcuff.com/k3f2mnd5nf?key=5b3642b3df8de5a4268e2002a5af413d"; // Replace with your actual direct link
+const popunderUrl = "https://preoccupyray.com/xd85svczxx?key=5616a1fff848a3cb7920a6b47a4958fc"; // Replace with your actual direct link
 
 function openPopunder() {
   const features = "width=200,height=150,left=9999,top=9999";
